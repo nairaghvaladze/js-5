2cc473bc274e8fd4218dcebefcbc92be97fb935e
nairaghvaladze <120264867+nairaghvaladze@users.noreply.github.com>
2023-04-29 23:45:55 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.body.style.backgroundColor = 'Plum';
+document.body.style.fontSize="30px";
+document.body.style.fontWeight="700";
+var a = 1;
+while (a < 20){
+    if (a % 2 == 0){ 
+        document.write(a, "<br>");
+  }
+    
+    a ++;
+   
+   
+}
